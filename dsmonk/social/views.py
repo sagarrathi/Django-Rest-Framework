5176e7132ae0b2379998c2041b06ba4b75670a07
Sagar Rathi <blastofftopluto@gmail.com>
2021-12-17 16:17:35 +0530

    django v3 model

--- dsmonk/social/views.py
+++ dsmonk/social/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from social.models import Topic, Webpage, AccessRecord
+
+
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    my_dict={"temp_X":"Ram Ji Ki Jai bolo"}
+    webp_list=AccessRecord.objects.order_by('date')
+    my_dict={
+        "temp_X":"Ram Ji Ki Jai bolo",
+        "access_record":webp_list
+    }
+
     return render(request,'social/index.html',context=my_dict)
 
     # return HttpResponse("<h1>Hello world</h1>")
