cc1ff019b8945582198bb3abc3b605b6aa6c0bbf
Sagar Rathi <blastofftopluto@gmail.com>
2021-12-17 13:29:15 +0530

    django v1

---
+++ resource/one.py
@@ -0,0 +1,11 @@
+def func():
+    print("fucn() in one.py")
+
+print("Top level one.py")
+
+if __name__=="__main__":
+    print("one.py is run directly")
+
+else:
+    print("one.py is been imported")
+    
