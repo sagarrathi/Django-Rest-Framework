6900fc520161da2c6d5bb940fa89a9900e113815
sagar rathi <blastofftopluto@gmail.com>
2022-01-13 17:02:30 +0530

    making registaion now

--- dsmonk/social/admin.py
+++ dsmonk/social/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from social.models import AccessRecord, Topic, Webpage, Users
 # Register your models here.
 
-
 admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Webpage)
 admin.site.register(Users)
+
+from social.models import UserProfileInfo
+
+admin.site.register(UserProfileInfo)
+# admin.site.register
--- dsmonk/social/models.py
+++ dsmonk/social/models.py
@@ -23,9 +23,19 @@ class AccessRecord(models.Model):
     def __str__(self):
         return str(self.date)
 
-class Users(models.Model):
+class Users  (models.Model):
     first_name=models.CharField(max_length=264)
     email_id=models.EmailField(max_length=254, unique=True, primary_key=True)
-
+    
     def __str__(self):
         return str(self.first_name+"::"+self.email_id)
+
+from django.contrib.auth.models import User
+
+class UserProfileInfo(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE)
+    portfolio=models.URLField(blank=True)
+    picture=models.ImageField(upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
