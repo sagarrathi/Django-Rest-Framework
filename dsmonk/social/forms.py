6b857ee797c05693eaed2bf4076efb443f19bfb6
sagar rathi <blastofftopluto@gmail.com>
2022-01-18 17:05:50 +0530

    added registarion with profile pic

--- dsmonk/social/forms.py
+++ dsmonk/social/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from django.core import validators
 from django.db.models.deletion import CASCADE
 from django.forms import fields, widgets
+
+
 from social import models
+from django.contrib.auth.models import User
+from social.models import UserProfileInfo
+
 
 def check_for_small(value):
     if len(value[0].lower()) <2:
@@ -10,6 +15,29 @@ def check_for_small(value):
 
 def check_for_same(value):
     pass
+
+class UserModelForm(forms.ModelForm):
+    class Meta:
+        model= models.Users
+        fields=['first_name', 'email_id']
+
+
+class UserForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model=User
+        fields=['username', 'email','password']
+    
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    portfolio =forms.URLField(required=True)
+    picture=forms.ImageField(required=True)
+
+    class Meta():
+        model=UserProfileInfo
+        fields=['portfolio','picture']
 
 # class UserForm(forms.Form):
 #     first_name=forms.CharField(validators=[check_for_small])
@@ -37,28 +65,3 @@ def check_for_same(value):
 #         if email !=vemail:
 #             raise forms.ValidationError("Email not matchin") 
 
-
-class UserModelForm(forms.ModelForm):
-    class Meta:
-        model= models.Users
-        fields=['first_name', 'email_id']
-
-
-from django.contrib.auth.models import User
-class UserForm(forms.ModelForm):
-    password=forms.CharField(widget=forms.PasswordInput())
-
-    class Meta():
-        model=User
-        fields=('username', 'email','password')
-    
-
-
-from social.models import UserProfileInfo
-class UserProfileInfoForm(forms.ModelForm):
-    portfolio =forms.URLField(required=True)
-    picture=forms.ImageField(required=True)
-
-    class Meta():
-        model=UserProfileInfo
-        exclude=('user',)
