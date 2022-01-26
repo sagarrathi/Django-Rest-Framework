77630d40bce4cca53eb80330a3cc8d5baf6bc867
Sagar Rathi <blastofftopluto@gmail.com>
2022-01-26 22:54:14 +0530

    adding blog v1 with some basic code

--- dsmonk/blog/models.py
+++ dsmonk/blog/models.py
@@ -1,3 +1,50 @@
+from statistics import mode
+from venv import create
 from django.db import models
 
+app_name="blog"
 # Create your models here.
+
+from django.utils import timezone
+from django.urls import reverse
+
+class Post(models.Model):
+    author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateField(default=timezone.now)
+    published_date=models.DateField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post=models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now())
+    approved_comments=models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment=True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("blog:post_list")
+
+    def __str__(self):
+        return self.text
+    
+# class Author(models.Model):
+#     name=
