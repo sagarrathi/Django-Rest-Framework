f506ea03f3cac9f12f324a03d2256440166a6330
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-01 17:22:25 +0530

    added props with true false value

--- vue-app/src/components/FriendContact.vue
+++ vue-app/src/components/FriendContact.vue
@@ -1,8 +1,15 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
-    <h6>Id:{{id}}</h6>
-    <button @click="toggle_detail()"> {{detail_switch ? 'Hide': 'Show'}} Details</button>
+    <h2>{{ name }} {{ is_missing_f  ? "(missing)" : "" }}</h2>
+    <h6>Id:{{ id }}</h6>
+    <button @click="toggle_missing()">
+      {{ is_missing_f  ? "Found" : "Missing" }}
+    </button>
+
+    <button @click="toggle_detail()">
+      {{ detail_switch ? "Hide" : "Show" }} Details
+    </button>
+
     <ul v-if="detail_switch">
       <li>
         <strong>{{ phone }}</strong>
@@ -16,22 +23,33 @@
 
 <script>
 export default {
-  props: [
-    'id',
-    'name',
-    'phone',
-    'email',
-  ], 
+  // props: ["id", "name", "phone", "email", "is_missing"],
+
+  props: {
+    id: String,
+    phone: { type: String, required: true },
+    name: { type: String, required: true },
+    email: String,
+    is_missing: { 
+      type: Boolean, 
+      required: false, 
+      default: false ,
+    },
+  },
 
   data() {
     return {
       detail_switch: false,
+      is_missing_f: this.is_missing,
     };
   },
 
   methods: {
     toggle_detail() {
       this.detail_switch = !this.detail_switch;
+    },
+    toggle_missing() {
+     this.is_missing_f = !this.is_missing_f 
     },
   },
 };
