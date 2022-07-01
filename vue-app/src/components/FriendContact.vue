7578ff28f9bbd4d701c8d0b6e4e0c8a6a71c2874
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-01 15:13:23 +0530

    adding vue app now

---
+++ vue-app/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggle_detail()"> {{detail_switch ? 'Hide': 'Show'}} Details</button>
+    <ul v-if="detail_switch">
+      <li>
+        <strong>{{ friend.phone }}</strong>
+      </li>
+      <li>
+        <strong>{{ friend.email }}</strong>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detail_switch: false,
+      friend: {
+        id: "ram",
+        name: "ram raghuvanshi",
+        phone: "0214",
+        email: "ram@bharat.in",
+      },
+    };
+  },
+
+  methods: {
+    toggle_detail() {
+      this.detail_switch = !this.detail_switch;
+    },
+  },
+};
+</script>
