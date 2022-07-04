c12533ac4d2f303549ab9f83754c52f955b08ee7
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-04 17:51:24 +0530

    adding manual dynamic components using v-f and @click

--- vue-app3/src/App.vue
+++ vue-app3/src/App.vue
@@ -1,50 +1,51 @@
 <template>
   <div>
     <the-header></the-header>
-    <base-card>
-      <badge-list></badge-list>
-    </base-card>
-    <base-card>
-      <user-info
-        :full-name="activeUser.name"
-        :info-text="activeUser.description"
-        :role="activeUser.role"
-      ></user-info>
-    </base-card>
-    <course-goals #default="slotProps">
-      <!-- <template #default="slotProps"> -->
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{slotProps['god']}}</p>
-      <!-- </template> -->
-      </course-goals>
+    <button @click="set_selected_component('active-goals')">Active Goals</button>
+    <button @click="set_selected_component('manage-goals')">Manage Goals</button>
+    <active-goals v-if="selected_component === 'active-goals'">
+    </active-goals>
+
+    <manage-goals v-if="selected_component === 'manage-goals'">
+    </manage-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BaseCard from "./components/BaseCard.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BaseCard from "./components/BaseCard.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    BaseCard,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // BaseCard,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
 
   data() {
     return {
+      selected_component:'active-goals',
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+    
   },
+  methods:{
+    set_selected_component(cmp){
+      this.selected_component=cmp;}
+  }
 };
 </script>
 
