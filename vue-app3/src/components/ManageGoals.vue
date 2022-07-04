5c753488469b71df3bd7941615008cfb4e3316f1
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-04 18:22:40 +0530

    adding  button in custom app and suing slot for styling error message  this way logic in app and rest in slot

--- vue-app3/src/components/ManageGoals.vue
+++ vue-app3/src/components/ManageGoals.vue
@@ -1,5 +1,39 @@
 <template>
-<h2>Manage Goals</h2>
-<label>Hi</label>
-<input type="text" />
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="set_goal">Set Goal</button>
+  <error-alert v-if="input_is_invalid">
+    <h2>Input Invalid</h2>
+    <p>Please enetr data</p>
+    <button @click="confirm_wrong">Ok</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "../components/ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+
+  data() {
+    return {
+      input_is_invalid: false,
+    };
+  },
+
+  methods: {
+    set_goal() {
+      const entered_value = this.$refs.goal.value;
+      if (entered_value === "") {
+        this.input_is_invalid = true;
+      }
+    },
+
+    confirm_wrong(){
+        this.input_is_invalid=false;
+    }
+  },
+};
+</script>
