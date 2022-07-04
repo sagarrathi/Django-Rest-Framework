9e602d69da319c97aedf7b927dfc19b54d6b24fc
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-04 14:25:38 +0530

    added local souricn g of componenet

--- vue-app3/src/components/BadgeList.vue
+++ vue-app3/src/components/BadgeList.vue
@@ -12,6 +12,17 @@
   </section>
 </template>
 
+
+<script>
+import BaseBadge from '../components/BaseBadge.vue';
+export default {
+  components:{
+    BaseBadge,
+  }
+}
+
+</script>
+
 <style>
 section h2 {
   margin: 0.5rem 0;
