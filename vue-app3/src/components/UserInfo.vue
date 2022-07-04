ea6eb71066267e9383cbcf4a5e27df220fa19235
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-04 16:53:57 +0530

    added slot for custom css only applied to composnent sharign same style

--- vue-app3/src/components/UserInfo.vue
+++ vue-app3/src/components/UserInfo.vue
@@ -1,35 +1,25 @@
 <template>
+  <!-- <base-card> -->
   <section>
-    <div>
-      <header>
-        <h3>{{ fullName }}</h3>
-      </header>
-      
+    <!-- <template v-slot:header> -->
+    <h3>{{ fullName }}</h3> 
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
+    <!-- </template> -->
     <p>{{ infoText }}</p>
   </section>
+  <!-- </base-card> -->
 </template>
 
 
 <script>
-
-
 export default {
   
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
 
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
+<style scoped>
 section header {
   display: flex;
   justify-content: space-between;
