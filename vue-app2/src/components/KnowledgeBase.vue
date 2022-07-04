44c633a57c6f2ea78ac48a0179d4a4b4ebf3b7ce
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-04 12:21:52 +0530

    added emit passthorigh multiple apps

--- vue-app2/src/components/KnowledgeBase.vue
+++ vue-app2/src/components/KnowledgeBase.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
     <h2>Select a Topic</h2>
-    <knowledge-grid :topics="topics" @select-topic="$emit('select-topic', $event)"></knowledge-grid>
+    <knowledge-grid 
+    :topics="topics" 
+    @select-topic="$emit('select-topic', $event)">
+    </knowledge-grid>
   </section>
 </template>
 
