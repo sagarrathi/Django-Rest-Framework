2743375a8169c38d3124ba2d1d67be46ea1068e6
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-05 14:01:58 +0530

    added page selector highlighter

--- vue-app4/src/components/learning-resources/TheResources.vue
+++ vue-app4/src/components/learning-resources/TheResources.vue
@@ -1,8 +1,14 @@
 <template>
   <base-card>
-    <base-button @click="set_selected_tab('stored-resources')">Stored Resources</base-button>
+    <base-button 
+    @click="set_selected_tab('stored-resources')"
+    :mode="stored_resource_button_mode"
+    >Stored Resources</base-button>
 
-    <base-button @click="set_selected_tab('add-resource')">Add Resource</base-button>
+    <base-button 
+    @click="set_selected_tab('add-resource')"
+    :mode="add_resource_button_mode"
+    >Add Resource</base-button>
   </base-card>
 
   <component :is="selected_tab"></component>
@@ -46,7 +52,20 @@ export default {
     }
   },
 
+  computed:{
+    stored_resource_button_mode(){
+      return this.selected_tab === 'stored-resources' ? null: 'flat';
+    },
+    
+    add_resource_button_mode(){
+      return this.selected_tab === 'add-resource' ? null: 'flat';
+    },
+    
+  },
+
   methods: {
+    
+
     set_selected_tab(val) {
       this.selected_tab = val;
     },
