cb8bef9d9409f7ebe811e364a25132c70760efe3
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-05 12:48:47 +0530

    added ui layout and learning-app with only css without ogic

---
+++ vue-app4/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resources"
+      :key="resource.id"
+      :resource="resource"
+    ></learning-resource>
+  </ul>
+</template>
+
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  components: {
+    'learning-resource': LearningResource,
+  },
+
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
