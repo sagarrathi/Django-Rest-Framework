383f5696c646c3eec62a73bd20dd5574deb9085a
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-08 15:44:10 +0530

    added http request to firebase

--- vue-apps/6_backend/src/components/survey/UserExperiences.vue
+++ vue-apps/6_backend/src/components/survey/UserExperiences.vue
@@ -3,9 +3,13 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="load_experiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="is_loading">Loading....</p>
+      <p v-if=" (results.length === 0 || !results )" >No strored Experice. Add survey</p>
+      <ul v-if="!is_loading && results &&results.length >0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +25,42 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+
+  data() {
+    return {
+      results: [],
+      is_loading: true,
+    };
+  },
+
+  methods: {
+    load_experiences() {
+      fetch('https://vue-http-d534a-default-rtdb.firebaseio.com/survey.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.is_loading = false;
+          console.log(data);
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted() {
+    this.load_experiences();
   },
 };
 </script>
