0a1d00207c74ee4c0897eb9bb0500c3e1804c18a
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-09 18:01:37 +0530

    added named routes

--- vue-apps/7_routing/src/main.js
+++ vue-apps/7_routing/src/main.js
@@ -13,10 +13,11 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/teams' },
     {
+      name:'teams',
       path: '/teams',
       component: TeamsList,
       children: [
-        { path: ':teamId', component: TeamMembers, props: true },
+        { name:'team-members', path: ':teamId', component: TeamMembers, props: true },
       ],
     },
 
