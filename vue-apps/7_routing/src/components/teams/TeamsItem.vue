ef23edc52fc6a4732640899f48a26b3cf02c87cd
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-09 17:30:11 +0530

    using props arather than route to get teamid

--- vue-apps/7_routing/src/components/teams/TeamsItem.vue
+++ vue-apps/7_routing/src/components/teams/TeamsItem.vue
@@ -2,13 +2,19 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <a href="#">View Members</a>
+    <router-link :to="team_member_link">View Members</router-link>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['name', 'memberCount'],
+  props: ['id','name', 'memberCount'],
+
+  computed: {
+    team_member_link(){
+      return '/teams/'+this.id
+    }
+  }
 };
 </script>
 
--- vue-apps/7_routing/src/main.js
+++ vue-apps/7_routing/src/main.js
@@ -4,12 +4,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component:TeamMembers, props:true},
+    // { path: '/teams/new'},
+
   ],
   linkActiveClass: 'active'
 });
