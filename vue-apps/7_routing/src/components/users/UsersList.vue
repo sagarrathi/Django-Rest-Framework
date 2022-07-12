140ae66a3a51f14d6185f63fd7678e72aaba823f
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-12 18:41:13 +0530

    added router meta data

--- vue-apps/7_routing/src/components/users/UsersList.vue
+++ vue-apps/7_routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="run_code">Confirm</button>
+  <button @click="save_changes">Save changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,14 +19,41 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return { changes_saved: false };
+  },
 
-  
   methods: {
     run_code() {
       console.log('Doing something Doing something');
       this.$router.push('/teams');
-
     },
+    save_changes() {
+      this.changes_saved = true;
+    },
+  },
+
+  beforeRouteEnter(to, from, next) {
+    console.log('User list CMP beforeRouteEnter');
+    console.log(to, from, next);
+    next();
+  },
+
+  beforeRouteLeave(to, from, next) {
+    console.log('before leaving page');
+    console.log(to, from, next);
+
+    if (this.changes_saved){
+      next();
+    } else {
+      
+      const user_want_to_leave= confirm("Are you share, yo got unsaved changes");
+      next(user_want_to_leave);}
+
+  },
+
+  unmounted() {
+    console.log('KIndly save data');
   },
 };
 </script>
--- vue-apps/7_routing/src/main.js
+++ vue-apps/7_routing/src/main.js
@@ -1,68 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import router from './router.js';
 
-import TeamsList from './components/teams/TeamsList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-
-import UsersList from './components/users/UsersList.vue';
-
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-
-import NotFound from './components/nav/NotFound.vue';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      components: {
-        default: TeamsList,
-        footer: TeamsFooter,
-      },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-    },
-
-    {
-      path: '/users',
-      components: {
-        default: UsersList,
-        footer: UsersFooter,
-      },
-    },
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(to, from, savedPosition) {
-    // console.log('to', to, 'from', from, 'saved', savedPosition);
-    if (savedPosition) {
-      return savedPosition;
-    }
-
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  console.log('Gloabl beforeEach');
-  console.log("to", to, "from", from,  "next", next);
-  // if (to.name=='team-members'){
-  //   next();
-  // } else {
-  //   next(   {name:'team-members', params: {teamId: 't2'}});
-  // }
-  next();  
-});
 
 const app = createApp(App);
 
