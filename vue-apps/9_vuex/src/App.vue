dc3a2943728deeec98707178060e4d0d6c8d2645
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-15 13:57:29 +0530

    changing state without mutataion

---
+++ vue-apps/9_vuex/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <base-container title="Vuex">
+  <the-counter></the-counter>
+  <button @click="addOne">Add 1</button>
+  <change-counter></change-counter>
+  </base-container>
+
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+    methods: {
+    addOne(){
+      this.$store.state.counter+=11;
+    },
+
+  },
+
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
