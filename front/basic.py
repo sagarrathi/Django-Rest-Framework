fde777314df1049044636b495c0b4f6d500d35e3
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-28 15:02:40 +0530

    using post will not work with django for csrf cookies hence we need dhjango rest frame work

--- front/basic.py
+++ front/basic.py
@@ -13,13 +13,12 @@ endpoint= "http://127.0.0.1:8000/api/"
 # get_response=requests.get(endpoint, data={"query":"Jai Shi Ram"}) # API
 
 # get_response=requests.get(endpoint, data={"query":"Jai Shi Ram"}) # API
-get_response=requests.get(endpoint) # API
-# get_response=requests.get(endpoint, params={'abc':123}, json={"query": "jai hanuman"}) # API
+# get_response=requests.get(endpoint) # API
+get_response=requests.post(endpoint,json={"query": "jai hanuman"}) # API
 
-
-print("headers",get_response.headers)
-print("text",get_response.text)  
-# print("json",get_response.json())
+# print("headers",get_response.headers)
+# print("text",get_response.text)  
+print("json",get_response.json())
 
 # print(get_response.status_code) 
  
