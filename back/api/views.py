21e42f2c8772d78c78af8ec82e67f5f89a063b7b
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-28 14:41:14 +0530

    added product seriliazer for properties

--- back/api/views.py
+++ back/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from product.models import Product
+from product.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
@@ -15,8 +16,8 @@ def api_home(request, *args, **kwargs):
     """
     if request.method  != 'GET':
         return Response({"detail":" Only GET allowed"}, status=405)
-    model_data=Product.objects.all().order_by("?").first()
+    instance=Product.objects.all().order_by("?").first()
     data={}
-    if model_data:
-        data=model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        data=ProductSerializer(instance).data
     return Response(data)
--- back/product/models.py
+++ back/product/models.py
@@ -8,3 +8,12 @@ class Product(models.Model):
     title=models.CharField(max_length=120)
     content=models.CharField(max_length=120, blank=True, null=True) 
     price=models.DecimalField(max_digits=12, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) *0.8)
+
+
+    def get_discount(self):
+        return "%.2f" %(0.8)
+    
