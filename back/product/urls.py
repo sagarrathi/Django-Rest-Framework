9a1afb4accbcfc57b7ee441e2f167dfa273f04a8
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-28 18:32:40 +0530

    added all type of view version 1

--- back/product/urls.py
+++ back/product/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.product_create_view),
+    path('', views.product_list_create_view),
     path('<int:pk>/', views.product_detail_view),
+    path('list/', views.product_list_view),
 ]
--- back/product/views.py
+++ back/product/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+#from django.htp import hhtp404
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
     # lookup_field  = 'pk
@@ -19,7 +22,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
             content=title          
         serializer.save(content=content)
 
-product_create_view=ProductCreateAPIView.as_view()
+product_list_create_view=ProductListCreateAPIView.as_view()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
@@ -31,3 +34,30 @@ product_detail_view=ProductDetailAPIView.as_view()
 
 
 
+class ProductListAPIView(generics.ListAPIView):
+    """
+    Not going to use this 
+    because we will use list with create
+    """
+    
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    # lookup_field  = 'pk
+
+product_list_view=ProductListAPIView.as_view()
+
+@api_view(["GET","POST"])
+def product_alt_view(request, *args, **kwargs):
+    method=request.method
+    if method=='GET':
+        # list data 
+        pass
+    if method=='POST':
+        serializer=ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True): #Raise expection provides frindly error
+            # serializer.save() this creates the instance
+            # ehile .data does not
+            print("serializer.data",serializer.data)
+            return Response(serializer.data)
+        else: 
+            return Response({"message":"title cannot be empty "})
