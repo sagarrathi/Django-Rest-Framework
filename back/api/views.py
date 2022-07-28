e61a86a107674dd0199c9a569496cfef8b172259
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-28 14:22:39 +0530

    added django rest serializer

--- back/api/views.py
+++ back/api/views.py
@@ -7,18 +7,16 @@ from rest_framework.response import Response
 
 from product.models import Product
 
-@api_view(["GET","POST"])
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """ 
 
     DRF API View 
     """
-
-    if request.method  != 'POST':
-        return Response({"detail":"GET not allowed"}, status=405)
+    if request.method  != 'GET':
+        return Response({"detail":" Only GET allowed"}, status=405)
     model_data=Product.objects.all().order_by("?").first()
     data={}
-    
     if model_data:
         data=model_to_dict(model_data, fields=['id', 'title', 'price'])
     return Response(data)
