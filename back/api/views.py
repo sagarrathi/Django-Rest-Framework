be5580b9f73b0c60471f0d246f5572adff71b873
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-28 14:03:37 +0530

    added django rest framework

--- back/api/views.py
+++ back/api/views.py
@@ -2,18 +2,23 @@ import json
 
 from django.forms.models import model_to_dict
 
-from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from product.models import Product
 
+@api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
+    """ 
+
+    DRF API View 
+    """
+
+    if request.method  != 'POST':
+        return Response({"detail":"GET not allowed"}, status=405)
     model_data=Product.objects.all().order_by("?").first()
     data={}
     
     if model_data:
-        data=model_to_dict(model_data, fields=['id', 'title'])
-        json_data_str=json.dumps(data)
-    print(data)    
-    return JsonResponse(data)
-
-    # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
+        data=model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
