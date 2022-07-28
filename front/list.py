0885c777f1cfb510e95639d69b1c9193d52c0cf7
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-28 18:32:40 +0530

    added all type of view version 1

---
+++ front/list.py
@@ -0,0 +1,14 @@
+import requests
+
+endpoint= "http://127.0.0.1:8000/api/products/"
+
+
+get_response=requests.get(endpoint) # API
+
+# print("headers",get_response.headers)
+# print("text",get_response.text)  
+print("json",get_response.json())
+# print(get_response.status_code) 
+ 
+
+
