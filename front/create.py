14fd9a07395dfb3ccd1a393aab76b34a6e1d741d
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-29 14:19:37 +0530

    genirc and minx view completed

--- front/create.py
+++ front/create.py
@@ -1,14 +1,14 @@
 import requests
 
-endpoint= "http://127.0.0.1:8000/api/products/"
+endpoint = "http://127.0.0.1:8000/api/products/"
 
 
-get_response=requests.post(endpoint,json={"title": "jai hanuman","price":100}) # API
+data = {"title": "Jai Ma Durga",
+        "price": 1100}
+
+get_response = requests.post(endpoint, json=data)  # API
 
 # print("headers",get_response.headers)
-# print("text",get_response.text)  
-print("json",get_response.json())
-# print(get_response.status_code) 
- 
-
-
+# print("text",get_response.text)
+print("json", get_response.json())
+# print(get_response.status_code)
