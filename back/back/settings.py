ad376ac5153e92021997c56afa3c62a5e60d1683
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-29 16:45:08 +0530

    added rest token

--- back/back/settings.py
+++ back/back/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'product',
-    'rest_framework',
+    
+
 ]
 
 MIDDLEWARE = [
