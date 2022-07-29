e99e939616d36492fa58f9d6115067151752b53e
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-29 12:14:35 +0530

    added create list and detail view in product_alt_view

--- back/product/views.py
+++ back/product/views.py
@@ -9,30 +9,32 @@ from .serializers import ProductSerializer
 # from django.http import Http404
 from django.shortcuts import get_object_or_404
 
+
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     # lookup_field  = 'pk
 
     def perform_create(self, serializer):
-        # print(serializer.validated_data)  
-        title= serializer.validated_data.get('title')
-        content=serializer.validated_data.get('content') or None
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
         print("content is none")
         if content is None:
-            content=title          
+            content = title
         serializer.save(content=content)
 
-product_list_create_view=ProductListCreateAPIView.as_view()
+
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     # lookup_field  = 'pk
 
-product_detail_view=ProductDetailAPIView.as_view()
 
+product_detail_view = ProductDetailAPIView.as_view()
 
 
 class ProductListAPIView(generics.ListAPIView):
@@ -40,36 +42,39 @@ class ProductListAPIView(generics.ListAPIView):
     Not going to use this 
     because we will use list with create
     """
-    
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     # lookup_field  = 'pk
 
-product_list_view=ProductListAPIView.as_view()
 
-@api_view(["GET","POST"])
-def product_alt_view(request,pk=None,  *args, **kwargs):
-    method=request.method
-    if method=='GET':
+product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None,  *args, **kwargs):
+    method = request.method
+    if method == 'GET':
         if pk is not None:
             # Detail
-            queryset=Product.objects.filter(pk=pk)
-            if not queryset.exist():
-                raise Http404
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
 
-            return Response()
-       
-        # list data 
-        queryset=Product.objects.all()
-        data=ProductSerializer(queryset, many=True).data
+        # list data
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
         return Response(data)
-    if method=='POST':
-        # make data
-        serializer=ProductSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True): #Raise expection provides frindly error
-            # serializer.save() this creates the instance
-            # ehile .data does not
-            print("serializer.data",serializer.data)
+
+    if method == 'POST':
+        # create data
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title=serializer.validated_data.get('title')
+            content=serializer.validated_data.get('content')
+            if content is None:
+                content=title
+            serializer.save(content=content)
             return Response(serializer.data)
-        else: 
-            return Response({"message":"title cannot be empty "})
+        
+        return Response({"message": "title cannot be empty "}, status=404)
