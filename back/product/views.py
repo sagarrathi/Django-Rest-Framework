70bf6c22447c2d09e29500985a55349bbc7921bf
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-29 12:45:18 +0530

    added  update and delete api

--- back/product/views.py
+++ back/product/views.py
@@ -22,7 +22,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         print("content is none")
         if content is None:
             content = title
-        serializer.save(content=content)
+        instance=serializer.save(content=content)
 
 
 product_list_create_view = ProductListCreateAPIView.as_view()
@@ -31,9 +31,7 @@ product_list_create_view = ProductListCreateAPIView.as_view()
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # lookup_field  = 'pk
-
-
+     # lookup_field  = 'pk
 product_detail_view = ProductDetailAPIView.as_view()
 
 
@@ -42,13 +40,32 @@ class ProductListAPIView(generics.ListAPIView):
     Not going to use this 
     because we will use list with create
     """
-
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field  = 'pk
-
-
 product_list_view = ProductListAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content=instance.title
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+product_delete_view = ProductDeleteAPIView.as_view()
 
 
 @api_view(["GET", "POST"])
@@ -70,11 +87,11 @@ def product_alt_view(request, pk=None,  *args, **kwargs):
         # create data
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            title=serializer.validated_data.get('title')
-            content=serializer.validated_data.get('content')
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
             if content is None:
-                content=title
+                content = title
             serializer.save(content=content)
             return Response(serializer.data)
-        
+
         return Response({"message": "title cannot be empty "}, status=404)
