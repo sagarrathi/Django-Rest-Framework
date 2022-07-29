b1f8d9679b0d9d0fa94cb4400beb91295ee77327
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-29 16:42:40 +0530

    adding cutom permsision completer with is_staff class

--- back/product/views.py
+++ back/product/views.py
@@ -17,7 +17,9 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes=[authentication.SessionAuthentication]
-    permission_classes=[IsStaffEditorPermission]
+    permission_classes=[permissions.IsAdminUser, IsStaffEditorPermission]
+
+    # permission_classes=[IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         # print(serializer.validated_data)
