4a47c5561ff0b2b00a95dd8035a81fdc75a3e1c9
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-30 16:50:24 +0530

    usisng rest framework validators

--- back/product/validators.py
+++ back/product/validators.py
@@ -1,5 +1,7 @@
-
 from rest_framework import serializers
+
+from rest_framework.validators import UniqueValidator
+
 from product.models import Product
 
 
@@ -12,3 +14,10 @@ def validate_title(value):
     if qs.exists():
         raise serializers.ValidationError(f"This: {value} is already present")
     return value
+
+def validate_title_2(value):
+    if "hanuman" not in value.lower():
+        raise serializers.ValidationError(f"Hanuman Ji needed in :{value}")
+    return  value
+
+unique_product_title=UniqueValidator(queryset=Product.objects.all())
