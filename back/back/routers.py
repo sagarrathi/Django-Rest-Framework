ff126f179e5f3570d739cc4d5347659d0a3ee28d
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-30 14:48:16 +0530

    adding router on rest framework which depend on viewset which is group of classes

---
+++ back/back/routers.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+
+from product.viewsets import ProductViewSet
+
+
+router = DefaultRouter()
+router.register('products-abc', ProductViewSet, basename='products')
+print(router.urls)
+
+urlpatterns = router.urls
