43d9242f3f73420a3102cafec76c389def124fb7
sagkgfs <sagar.rathi@dvarakgfs.com>
2022-07-30 15:30:44 +0530

    adding url in serilaize to acces the object deyail and create

--- back/product/serializers.py
+++ back/product/serializers.py
@@ -5,13 +5,24 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only=True)
+    # url = serializers.SerializerMethodField(read_only=True)
+    url=serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
 
+    )
+
+    edit_url=serializers.HyperlinkedIdentityField(
+        view_name='product-edit',
+        lookup_field='pk',
+
+    )
     class Meta:
         model = Product
         fields = [
             'pk',
             'url',
+            'edit_url',
             'title',
             'content',
             'price',
@@ -21,12 +32,12 @@ class ProductSerializer(serializers.ModelSerializer):
     # Obj is instance of object being called
     # Thuis prevents dynamic instacne whihc then have to be sourced
 
-    def get_url(self, obj):
-        request = self.context.get('request')
-        if request is None:
-            return None
-        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
-        # return f"/api/products/{obj.pk}/"
+    # def get_url(self, obj):
+    #     request = self.context.get('request')
+    #     if request is None:
+    #         return None
+    #     return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+    #     # return f"/api/products/{obj.pk}/"
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
