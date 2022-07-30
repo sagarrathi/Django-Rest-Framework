6cd7acde61135fe207b466cf1a419f6eaaa88b98
Sagar Rathi <blastofftopluto@gmail.com>
2022-07-30 15:30:44 +0530

    adding url in serilaize to acces the object deyail and create

--- back/product/urls.py
+++ back/product/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
     # path('', views.product_list_create_view),
     path('', views.product_list_create_view),
-    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     path('<int:pk>/delete/', views.product_delete_view),
     path('<int:pk>/', views.product_detail_view, name='product-detail'),
 ]
