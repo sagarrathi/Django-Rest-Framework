80016575728c1bf86be5799853b36f009bd1a088
Sagar Rathi <blastofftopluto@gmail.com>
2022-08-02 17:56:09 +0530

    added code for getitng user form model

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,4 +1,9 @@
+#Rest
 from rest_framework import serializers
+
+#Django
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 class UserProductInlineSerializer(serializers.Serializer):
     url = serializers.HyperlinkedIdentityField(
@@ -13,10 +18,10 @@ class UserProductInlineSerializer(serializers.Serializer):
 class UserPublicSerializer(serializers.Serializer):
     username=serializers.CharField(read_only=True)
     id=serializers.IntegerField(read_only=True)
-    other_product=serializers.SerializerMethodField(read_only=True)
+    # other_product=serializers.SerializerMethodField(read_only=True)
 
-    def get_other_product(self, obj):
-        request=self.context.get('request')
-        user=obj
-        my_products_qs=user.product_set.all()
-        return UserProductInlineSerializer(my_products_qs, many=True, context=self.context).data
+    # def get_other_product(self, obj):
+    #     request=self.context.get('request')
+    #     user=obj
+    #     my_products_qs=user.product_set.all()
+    #     return UserProductInlineSerializer(my_products_qs, many=True, context=self.context).data
