6a236dfe4d818d908b76b9851a6edd45f1b82745
Sagar Rathi <blastofftopluto@gmail.com>
2022-08-02 17:31:50 +0530

    passing context for url genration

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
 class UserProductInlineSerializer(serializers.Serializer):
-    # url = serializers.HyperlinkedIdentityField(
-    #     view_name='product-detail',
-    #     lookup_field='pk',
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
 
-    # )
+    )
     title = serializers.CharField(read_only=True)
 
 
@@ -16,6 +16,7 @@ class UserPublicSerializer(serializers.Serializer):
     other_product=serializers.SerializerMethodField(read_only=True)
 
     def get_other_product(self, obj):
+        request=self.context.get('request')
         user=obj
         my_products_qs=user.product_set.all()
-        return UserProductInlineSerializer(my_products_qs, many=True).data
+        return UserProductInlineSerializer(my_products_qs, many=True, context=self.context).data
