15b4094bf741b88c56cb848a5dd19f81355e6e25
Sagar Rathi <blastofftopluto@gmail.com>
2022-08-05 14:44:41 +0530

    trying to understand manager and queryset in models

--- back/product/models.py
+++ back/product/models.py
@@ -1,14 +1,37 @@
+# Django
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
+
 
 # Create your models here.
 User =settings.AUTH_USER_MODEL #auth.user
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+
+    def search(self, query, user=None):
+        lookup= Q(title__icontains=query)
+        qs=self.filter(lookup)
+        if user is not None:
+            qs=qs.filter(user=user)
+        return qs
+
+class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ProductQueryset(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().is_public().search(query, user=user)
 
 class Product(models.Model):
     user=models.ForeignKey(User, default=1,null=True,  on_delete=models.SET_NULL) 
     title=models.CharField(max_length=120)
     content=models.CharField(max_length=120, null=True) 
     price=models.DecimalField(max_digits=12, decimal_places=2, default=99.99)
+
+    public=models.BooleanField(default=True)
 
     @property
     def sale_price(self):
